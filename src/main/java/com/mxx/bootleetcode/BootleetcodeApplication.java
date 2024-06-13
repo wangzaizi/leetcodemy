7c082b87877bc3277c87fdd32bb4b72261ed1508
weipengcheng <weipengcheng123>
2024-06-13 18:04:47 +0800

    初始化

---
+++ src/main/java/com/mxx/bootleetcode/BootleetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.mxx.bootleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootleetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootleetcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mxx/bootleetcode/BootleetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxx.bootleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootleetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
